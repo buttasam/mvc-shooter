95633850db8f09ebacd793ab337adc28d8f415e2
Samuel Butta <samuel@butta.cz>
2017-12-05 18:30:17 +0100

    StaticEnemyStrategy implements serializable

--- src/main/java/cz/fit/dpo/mvcshooter/model/strategy/enemy/StaticEnemyStrategy.java
+++ src/main/java/cz/fit/dpo/mvcshooter/model/strategy/enemy/StaticEnemyStrategy.java
@@ -1,9 +1,11 @@
 package cz.fit.dpo.mvcshooter.model.strategy.enemy;
 
+import java.io.Serializable;
+
 /**
  * @author Samuel Butta
  */
-public class StaticEnemyStrategy implements EnemyStrategy {
+public class StaticEnemyStrategy implements EnemyStrategy, Serializable {
 
     @Override
     public int calculateX(int x, int xCounter, int xDirection) {
